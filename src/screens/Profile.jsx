f77773d2123d88d939508d8ed9922e4c67a4a6e3
saarangbond <48871052+saarangbond@users.noreply.github.com>
2020-08-11 13:07:50 -0700

    mostly finished profile page work

--- src/screens/Profile.jsx
+++ src/screens/Profile.jsx
@@ -13,23 +13,38 @@ import {
 import firebase from '../firebase.js';
 
 import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap'
+import { isThrowStatement } from 'typescript';
 
 export default class Profile extends Component {
     constructor(props) {
         super(props)
         this.classes = ["Math", "Geometry", "Algebra", "Trigonometry", "Calculus", "Science", "Biology", "Chemistry", "Physics", "English", "Survey", "AP Comp", "History", "World Studies", "AP Euro", "WHAP", "USHAP", "Spanish", "Anime", "Chinese", "Computer Science", "Art", "Music"];
-
         this.state = {
             selected: [],
             user: {
                 auth: null,
                 name: "Anonymous",
-            }
+            },
+            userClasses: [],
         }
-
+        this.setUserClasses();
         this.handleInputChange = this.handleInputChange.bind(this);
     }
 
+    setUserClasses(){
+        firebase.auth().onAuthStateChanged(user => {
+            if (user) {
+                this.setState({ user: { auth: user, name: user.displayName } })
+                //console.log(this.state.user.auth)
+                firebase.firestore().collection("users").doc(this.state.user.auth.uid).get().then(doc => {
+                    this.setState({userClasses: doc.data().classes});
+                })
+                
+            } else {
+                this.setState({ user: { auth: user, name: 'Anonymous' } })
+            }
+        });
+    }
     componentDidMount() {
         firebase.auth().onAuthStateChanged(user => {
             if (user) {
@@ -52,6 +67,7 @@ export default class Profile extends Component {
                     // console.log(userdata[0].data())
                     // console.log(doc.data().classes)
                 })
+                
             } else {
                 this.setState({ user: { auth: user, name: 'Anonymous' } })
             }
@@ -61,26 +77,31 @@ export default class Profile extends Component {
     handleInputChange(event) {
         const target = event.target;
         var value = target.value;
-
         console.log(target.checked && !(this.state.selected.includes(target.name)));
 
         if (target.checked && !(this.state.selected.includes(target.name))) {
             this.state.selected.push(target.name);
+            console.log("pushed elem", this.state.selected)
+            target.value = true;
         } else {
             const index = this.state.selected.indexOf(target.name);
             if (index > -1) {
                 this.state.selected.splice(index, 1);
+                console.log("remove elem", this.state.selected)
             }
+            target.value = false;
         }
-        this.setState();
+        this.setState({update: 0});
     }
 
     submitHandler = (event) => {
         event.preventDefault();
-
-        firebase.firestore().collection("users").doc(this.state.user.auth.uid).set({
+        
+        firebase.firestore().collection("users").doc(this.state.user.auth.uid).update({
             classes: this.state.selected,
-        }, { merge: true });
+        });
+        this.setUserClasses()
+        this.setState({update: 0});
     }
 
     render() {
@@ -91,16 +112,23 @@ export default class Profile extends Component {
                         <Link to="/">Home</Link>
                         <h1>Profile</h1>
                     </div>
+                    <div id="checkBoxTitle">
+                        <a href="#">Select Classes: <span className="badge"> {this.state.selected.length}</span></a>
+                        <br/>
+                    </div>
                     <div id="checkBoxSelect">
                         <Form onSubmit={this.submitHandler} >
                             <FormGroup check>
+                                {
+                                    //console.log(this.state.userClasses)
+                                }
                                 {
                                     this.classes.map(cless => {
                                         return (
                                             <React.Fragment>
                                                 <div className="tickBoxSurround">
                                                     <Label for={cless} >
-                                                        <Input onChange={this.handleInputChange} classname="tickboxes" id={cless} name={cless} type="checkbox" />
+                                                        <Input onChange={this.handleInputChange} className="tickboxes" id={cless} name={cless} type="checkbox" checked={this.state.userClasses.indexOf(cless) > -1}/>
                                                         {cless}
                                                     </Label>
                                                     <br />
@@ -111,7 +139,7 @@ export default class Profile extends Component {
                                 }
                             </FormGroup>
 
-                            <Button color="info" id="submitClasses">Submit</Button>
+                            <Button color="info" id="submitClasses">Save Classes</Button>
                         </Form>
                     </div>
                 </div>
